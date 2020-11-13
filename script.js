af449aa60a5e4cf1c75d58a199ab61559577e2ca
Daniel McKinney <adrmckinney@gmail.com>
2020-11-13 15:45:12 -0500

    finished steps 0-3

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Add validation!');
+const form = document.querySelector('#parking-form')
+const cost = document.querySelector('#days')
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    validate()
+})
+
+cost.addEventListener('InputEvent', function(event) {
+    let numberOfDays = event.target.innerText
+    let result = eval(numberOfDays * 5)
+    console.log(result)
+})
